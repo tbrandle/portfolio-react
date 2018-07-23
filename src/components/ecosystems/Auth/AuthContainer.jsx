59c26eac87363187e897cc6765788912d9371edc
Tim Brandle <tim@bowtie.co>
2018-07-23 12:56:02 -0600

    change vscode settings to never rename imports

---
+++ src/components/ecosystems/Auth/AuthContainer.jsx
@@ -0,0 +1,43 @@
+// Containers should include all logic that enhances a component
+// this includes any reduce methods, recompose, or middleware.
+
+import { compose, withState, withHandlers } from 'recompose'
+import { withEither } from '@bowtie/react-utils'
+import Auth from './Auth'
+import { Loading } from 'atoms'
+
+export const enhance = compose(
+  withState('isComponentLoading', 'setIsComponentLoading', false),
+  withHandlers({
+    handleLoginSubmit: ({ setIsComponentLoading, history }) => (formData) => {
+      console.log('formData', formData)
+      setIsComponentLoading(true)
+
+      auth.login(formData)
+      .then((user) => {
+        setIsComponentLoading(false)
+        console.log(user)
+        history.push('/home')
+      })
+      .catch(resp => {
+        setIsComponentLoading(false)
+        console.log(resp)
+      })
+    },
+    handleLogout: ({ history }) => () => {
+      console.log('logged out');
+      // auth.logout()
+      //   .then(resp => {
+      //     console.log('Signed Out')
+      //     history.push('/login')
+      //   })
+      //   .catch(resp => {
+      //     console.log('Error Signed Out')
+      //   })
+    }
+  }),
+  withEither(loadingConditionFn, Loading)
+)
+
+export default enhance(Auth)
+
